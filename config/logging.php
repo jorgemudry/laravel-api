981766edb30943e8d849206820c211261de0a3a0
Jorge Mudry <jorgemudry@gmail.com>
2023-02-18 02:10:17 -0300

    Add Bugsnag

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,10 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'bugsnag' => [
+            'driver' => 'bugsnag',
+        ],
     ],
 
 ];
