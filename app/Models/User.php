d9ea4e82594a14c188cc2fa11eaaff3222364779
Jorge Mudry <jorgemudry@gmail.com>
2023-02-18 22:25:04 -0300

    Add pre-commit and post-merge git hooks and their installer

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ *
  */
 class User extends Authenticatable
 {
