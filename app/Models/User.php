b37224ffcff6cdc1a78765e152bd3a0a0b1be129
Jorge Mudry <jorgemudry@gmail.com>
2023-02-19 03:34:20 -0300

    Fix code styling

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,6 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
- *
  */
 class User extends Authenticatable
 {
