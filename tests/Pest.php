dee4b13cd4e28fe79500d0ddabfac2517e82ecaf
Jorge Mudry <jorgemudry@gmail.com>
2023-03-05 19:06:19 -0300

    Some tweaks on tests setup

--- tests/Pest.php
+++ tests/Pest.php
@@ -17,8 +17,7 @@ declare(strict_types=1);
 
 uses(
     Tests\TestCase::class,
-    // Illuminate\Foundation\Testing\RefreshDatabase::class,
-)->in('Feature');
+)->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
