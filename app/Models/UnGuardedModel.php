182fbdb112f0fb9c168a747e244e3a47f3d8ee4d
Jorge Mudry <jorgemudry@gmail.com>
2023-03-06 00:47:07 -0300

    Customize some commonly used stubs

--- app/Models/UnGuardedModel.php
+++ app/Models/UnGuardedModel.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UnGuardedModel extends Model
 {
     use HasFactory;
+    use HasUlids;
 
     /**
      * List of attributes for which mass assignment is forbidden.
