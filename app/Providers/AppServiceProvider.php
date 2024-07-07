2a1569241fe9731a052d3cd78b9776a1f4b5f5c0
Jorge Mudry <jorgemudry@gmail.com>
2024-07-07 18:39:26 -0300

    Force strict mode for models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Model::shouldBeStrict(app()->isProduction() === false);
         RateLimiter::for(
             'api',
             fn (Request $request) => Limit::perMinute(60)->by($request->user()?->id ?: $request->ip())
