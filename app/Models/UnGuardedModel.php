d0d747d1f635c9cc01be256e2ce0096dc4f06f3f
Jorge Mudry <jorgemudry@gmail.com>
2024-07-18 13:52:51 -0300

    Set user's model factory type and update composer dependencies

--- app/Models/UnGuardedModel.php
+++ app/Models/UnGuardedModel.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UnGuardedModel extends Model
 {
-    use HasFactory;
     use HasUlids;
 
     /**
